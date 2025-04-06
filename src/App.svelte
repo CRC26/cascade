452eb9d889a643d3e3f543073572832a6c6d87a8
cara cahill <caracahill@outlook.com>
2025-04-06 15:59:54 +0100

    Project

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<div class="container">
+  <div class="box has-text-centered">
+    <div class="title"> Cascade</div>
+    <div class="subtitle">Addressing payments and messaging for primary and secondary schools</div>
+  </div>
+  <div class="container">
+    <div class="box has-text-centered">
+    <img alt="cara" src="images/cara.jpg " width="100" />
+    <li> Student Name: Cara Cahill </li> 
+    <li> Student Number: 20099855</li>
+    <li> Project Description: Cascade is a web application developed as a final year project for the SETU HDIP in computer science. 
+    Virtue aims to streamline communication and payments between primary schools and parents with accessibility and financial 
+    constraints in mind. Virtue will work to remove the numerous mobile applications needed for communication and payments
+    between schools and parents.</li>
+    </div>
+  </div>
+</div>
+
